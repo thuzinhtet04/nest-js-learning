8c942556e6a7f7a6001b489c1a1a6aded97de567
thuzinhtet <thuzinhtet.dev@gmail.com>
2025-12-31 07:40:47 +0630

    dockers

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,23 +1,17 @@
-import { User } from 'src/users/users.entity';
 import {
   BadRequestException,
   Body,
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { LoginUserDTO } from 'src/users/dto/login-user.dto';
 import { UsersService } from 'src/users/users.service';
-import { Repository } from 'typeorm';
 import * as bcrypt from 'bcryptjs';
-import { retry } from 'rxjs';
+
 @Injectable()
 export class AuthService {
-  constructor(
-    @InjectRepository(User) private userRepo: Repository<User>,
-    private readonly userService: UsersService,
-  ) {}
+  constructor(private readonly userService: UsersService) {}
   signup(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
   }
